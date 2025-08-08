0402b84d93ba0069c4a3dfbafa4058d7c77942b6
MURTUZA <murt.mim@gmail.com>
2025-08-08 22:51:57 +0530

    SSO Authentication class

---
+++ login/src/main/java/com/example/SSOAuthentication.java
@@ -0,0 +1,8 @@
+package com.example;
+public class SSOAuthentication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the SSO Authentication System");
+        // Here you can add more functionality related to SSO authentication
+    }
+
+}

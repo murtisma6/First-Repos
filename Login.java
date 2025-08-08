fd122d5238cf2f02e48c0e61332e0961791164df
MURTUZA <murt.mim@gmail.com>
2025-08-08 17:44:49 +0530

    Add basic Login system welcome message

---
+++ Login.java
@@ -0,0 +1,8 @@
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Login System");
+        // Here you can add more functionality related to login 
+
+
+    }
+}
